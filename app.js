cab4a6257c70904417daf39e2c70929410417c3a
Papayaweb <161533929+Papayaweb@users.noreply.github.com>
2025-01-08 14:04:38 +0000

    gestion-tache JS AVANCE

---
+++ app.js
@@ -0,0 +1,50 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.querySelector('#taskInput');
+    const addTaskBtn = document.querySelector('#addTaskBtn');
+    const taskList = document.querySelector('#taskList');
+    // Charger les tâches depuis localStorage
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    // Afficher les taches depuis le localStorage
+    tasks.forEach((task) => {
+        ajouterTache(task);
+    })
+
+    // fontion qui permet d'afficher la liste des taches 
+    function ajouterTache(tache, e) {
+        const li = document.createElement('li');
+        if (tache !== '') {
+            li.innerText = tache;
+        } else {
+            e.preventDefault();
+        }
+        // console.log(li);
+        taskList.append(li);
+    }
+
+    // button ecouteurs d'evenement executant les fonctions
+    addTaskBtn.addEventListener('click', (e) => {
+        var tache = taskInput.value;
+        const k = tasks.push(tache)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        ajouterTache(tache)
+        taskInput.value = '';
+        addMultipleTasks(tasks);
+    })
+
+
+    // Fonction qui accepte plusieurs noms de tâches et 
+    // les ajoute toutes à la liste
+    function addMultipleTasks(...newTasks) {
+        const t = [...tasks, ...newTasks];
+        localStorage.setItem('tasks', JSON.stringify(t));
+        newTasks.array.forEach(task => displayTask(task));
+    }
+    
+    // Utilisation de la destructuration pour afficher 
+    // les propriétés d'une tâche dans la console
+    if (tasks.length > 0) {
+        const [firstTask, ...restTasks] = tasks;
+        console.log('Premiere tache :', firstTask);
+        console.log('Reste des taches :', restTasks);
+    }
+})
